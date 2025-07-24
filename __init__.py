a2865b2b90212ac0ac1234e1166aa11e9ff9a329
zaqxs123456 <56287342+zaqxs123456@users.noreply.github.com>
2025-07-25 03:03:28 +0800

    refactor: remove commented-out code and unused imports in __init__.py for clarity

--- __init__.py
+++ __init__.py
@@ -18,17 +18,10 @@ Usage:
     ## Access raw configuration
     raw_data = cm.raw_config
 """
-# import os
-# import sys
-
-
-# SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
-# sys.path.append(os.path.dirname(SCRIPT_DIR))
 
 try:
     from .config_manager import ConfigManager
 except ImportError:
     from config_manager import ConfigManager
+    
 cm = ConfigManager(verbose=False)
-
-# __all__ = ['ConfigManager', 'ConfigKeysGenerator']
