9f484d983c80bafdfa7d209b2480cf9968ee8281
zaqxs123456 <56287342+zaqxs123456@users.noreply.github.com>
2025-07-25 02:53:12 +0800

    refactor: comment out unused imports and code in __init__.py for clarity

--- __init__.py
+++ __init__.py
@@ -18,17 +18,17 @@ Usage:
     ## Access raw configuration
     raw_data = cm.raw_config
 """
-import os
-import sys
+# import os
+# import sys
 
 
-SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
-sys.path.append(os.path.dirname(SCRIPT_DIR))
+# SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
+# sys.path.append(os.path.dirname(SCRIPT_DIR))
 
-try:
-    from .config_manager import ConfigManager, ConfigKeysGenerator
-except ImportError:
-    from config_manager import ConfigManager, ConfigKeysGenerator
-cm = ConfigManager(verbose=False)
+# try:
+#     from .config_manager import ConfigManager, ConfigKeysGenerator
+# except ImportError:
+#     from config_manager import ConfigManager, ConfigKeysGenerator
+# cm = ConfigManager(verbose=False)
 
-__all__ = ['ConfigManager', 'ConfigKeysGenerator']
+# __all__ = ['ConfigManager', 'ConfigKeysGenerator']
